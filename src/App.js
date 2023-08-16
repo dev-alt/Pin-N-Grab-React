f63f13af377a916de5eb077fabfa1394c219cd78
Andrew <andrew.logan@hotmail.com>
2023-08-16 20:09:58 +1200

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AdminPage from './pages/AdminPage';
+import { BrowserRouter as Router, Route, Outlet, Link, Routes } from 'react-router-dom';
+import { SignIn } from './components/Member/SignIn'; // Update the import as mentioned earlier
+import { Nav, Navbar } from './components/Common/NavBar';
+import { HomePage } from './pages/HomePage'; // Import the HomePage component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Navbar />        
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
+
   );
 }
+
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function HomePage() {
+  return (
+    <div>
+      <h2>Welcome to My App</h2>
+      <p>This is the home page of the app. Feel free to explore!</p>
+    </div>
+  );
+}
