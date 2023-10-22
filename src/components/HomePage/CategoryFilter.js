cb47f4407512a00265138bd72fe941d1a8e9013a
AticeRuan <rainryy@gmail.com>
2023-10-22 21:02:02 +1300

    v.0.1

--- src/components/HomePage/CategoryFilter.js
+++ src/components/HomePage/CategoryFilter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Tooltip } from '@mui/material';
 import { IconButton } from '@mui/material';
 import {
   ElectricalServices,
@@ -21,38 +21,35 @@ const CategoryFilter = ({
   return (
     <Box>
       <IconButton
-        color={selectedCategories.includes(1) ? 'secondary' : 'primary'}
-        onClick={() => toggleCategory(1)}
-      >
+        color={selectedCategories.includes(1) ? 'primary' : '#BFC1C4'}
+        onClick={() => toggleCategory(1)}>
         <ElectricalServices sx={iconStyle} />
       </IconButton>
       <IconButton
-        color={selectedCategories.includes(2) ? 'secondary' : 'primary'}
-        onClick={() => toggleCategory(2)}
-      >
+        color={selectedCategories.includes(2) ? 'primary' : '#BFC1C4'}
+        onClick={() => toggleCategory(2)}>
         <LocalFlorist sx={iconStyle} />
       </IconButton>
       <IconButton
-        color={selectedCategories.includes(3) ? 'secondary' : 'primary'}
-        onClick={() => toggleCategory(3)}
-      >
+        color={selectedCategories.includes(3) ? 'primary' : '#BFC1C4'}
+        onClick={() => toggleCategory(3)}>
         <LocalShipping sx={iconStyle} />
       </IconButton>
       <IconButton
-        color={selectedCategories.includes(4) ? 'secondary' : 'primary'}
-        onClick={() => toggleCategory(4)}
-      >
+        color={selectedCategories.includes(4) ? 'primary' : '#BFC1C4'}
+        onClick={() => toggleCategory(4)}>
         <FormatPaintIcon sx={iconStyle} />
       </IconButton>
       <IconButton
-        color={selectedCategories.includes(5) ? 'secondary' : 'primary'}
-        onClick={() => toggleCategory(5)}
-      >
+        color={selectedCategories.includes(5) ? 'primary' : '#BFC1C4'}
+        onClick={() => toggleCategory(5)}>
         <Build sx={iconStyle} />
       </IconButton>
-      <IconButton color="primary" onClick={handleClearFilters}>
-        <Clear />
-      </IconButton>
+      <Tooltip title="clear filter">
+        <IconButton color="primary" onClick={handleClearFilters}>
+          <Clear />
+        </IconButton>
+      </Tooltip>
     </Box>
   );
 };
